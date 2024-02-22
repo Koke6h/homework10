0984c91bc756e8029bca09e3afbcaff461f32c6b
User <git@github.com:Koke6h/helloworld.git>
2024-02-22 20:10:44 +0400

    finished hw10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = lastName+firstName+middleName;
+        System.out.println("ФИО сотрудника - "+fullName);
+    }
+    public  static  void task2(){
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullNameLower = lastName+firstName+middleName;
+        String fullName = fullNameLower.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —  "+fullName);
+    }
+    public static void task3(){
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+        String fullName2 = lastName+firstName+middleName;
+        String fullName = fullName2.replace("ё","е");
+        System.out.println("Данные Ф. И. О. сотрудника — "+fullName);
     }
 }
